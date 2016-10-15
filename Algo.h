f88182a3fafe5f35f7bf2284560a6860f72112df
EML16 <evanmlyon@gmail.com>
2016-10-15 09:02:39 -0700

    Add Algo.h header file via upload
    Added Algo.h. forgot header file on original upload

---
+++ Algo.h
@@ -0,0 +1,3 @@
+#include<vector>
+void Enumeration(const std::vector<int> &arr, int &startIdx, int &endIdx, int &sum);
+void ImprovedEnumeration(const std::vector<int> &arr, int &startIdx, int &endIdx, int &sum);
