c19788c11039070ad222f4e69332283fed87ae5d
EML16 <evanmlyon@gmail.com>
2016-10-15 14:22:36 -0700

    Add files via upload

--- Algo.h
+++ Algo.h
@@ -1,3 +1,3 @@
 #include<vector>
-void Enumeration(const std::vector<int> &arr, int &startIdx, int &endIdx, int &sum);
-void ImprovedEnumeration(const std::vector<int> &arr, int &startIdx, int &endIdx, int &sum);
+void Enumeration(const std::vector<int> &arr,  int &startIdx,  int &endIdx,  int &sum);
+void ImprovedEnumeration(const std::vector<int> &arr,  int &startIdx,  int &endIdx,  int &sum);
